28353a9dcb67e72a095880aee66fb4b8fc216279
alifpdf <alif.rafayel@hotmail.fr>
2026-03-09 16:32:03 +0100

    changment

---
+++ fichier copy.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+  int *tab = malloc(10 * sizeof(int));
+  if (tab == NULL) {
+    return 1;
+  }
+
+  printf(a);
+
+  tab[0] = 42;
+  printf("%d\n", tab[0]);
+
+  /* fuite mémoire: on oublie free(tab); */
+  return 0;
+}
