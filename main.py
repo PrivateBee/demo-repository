ac8f333d5d939f8c047b0692852a8279a6df1dcf
alifalis <104689782+alifalis@users.noreply.github.com>
2026-03-25 10:48:18 +0100

    Fix docstring formatting in main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-"""
-Module d'exemple permettant d'afficher des salutations.
-"""
+"""Module d'exemple permettant d'afficher des salutations."""
+
 
 def hello() -> None:
-    """
-    Affiche le message 'salut' dans la console de sortie.
-    """
+    """Affiche le message 'salut' dans la console de sortie."""
     print("salut")
+
 
 if __name__ == "__main__":
     hello()
