7390a3a1a20b2951a8e8e4af7dfa57731e0cdf39
alifalis <alif.rafayel@zohomail.eu>
2026-04-09 11:48:29 +0200

    test java[java]

---
+++ java/src/Ballon.java
@@ -0,0 +1,38 @@
+package java.src;
+
+public class Ballon {
+    private String nom;
+    private double taille;
+
+    // Constructeur
+    public Ballon(String nom, double taille) {
+        this.nom = nom;
+        this.taille = taille;
+    }
+
+    // Getter pour nom
+    public String getNom() {
+        return nom;
+    }
+
+    // Setter pour nom
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    // Getter pour taille
+    public double getTaille() {
+        return taille;
+    }
+
+    // Setter pour taille
+    public void setTaille(double taille) {
+        this.taille = taille;
+    }
+
+    // Méthode pour afficher les informations
+    public void afficher() {
+        System.out.println("Nom : " + nom);
+        System.out.println("Taille : " + taille);
+    }
+}
---
+++ java/src/Main.java
@@ -0,0 +1,7 @@
+package java.src;
+public class Main {
+    public static void main(String[] args) {
+        Ballon ballon = new Ballon("Ballon de foot", 22.5);
+        ballon.afficher();
+    }
+}
