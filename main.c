1c19be9a58b21e6ee1f5bbe2ad0797ce30cd1ce4
alifalis <104689782+alifalis@users.noreply.github.com>
2026-05-06 10:51:58 +0200

    Refactor main.c to remove malloc and print greeting
    Removed dynamic memory allocation and added a greeting.

--- main.c
+++ main.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
-  int *tab = malloc(10 * sizeof(int));
-  if (tab == NULL) {
-    return 1;
-  }
-
-  tab[0] = 42;
-  printf("%d\n", tab[0]);
-
+  printf("Bonjour\n");
   return 0;
 }
